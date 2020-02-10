55b7b1f163695077dcb1378e25e2fef73652d924
sistemasAA <sistemas@aaglobalinvest.com>
2020-02-10 16:52:55 -0500

    process.php modified 1

--- process.php
+++ process.php
@@ -1,5 +1,9 @@
 <?php
 
+    for($i=0;$i<=1000000;$i++){
+        echo ".";
+    }
+
     function retornar(){
         $val = ['val' => 1,'val2' => 2];
         echo $_POST['first']." ".$_POST['last']." ".$_POST['handle']."\n";
